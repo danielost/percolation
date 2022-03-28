a814e770e64deaabb976a1450eca8d93ccd1148c
Daniel <danya.ostap@gmail.com>
2022-03-28 16:22:57 +0300

    added tests to Main class

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        Percolation prc = new Percolation(4);
-        prc.open(1, 3);
-        prc.open(2, 1);
-        prc.open(2, 2);
-        prc.open(2, 4);
-        prc.open(3, 2);
-        prc.open(3, 3);
-        prc.open(4, 3);
-        prc.open(4, 4);
-
-        //
-        prc.open(2, 3);
-        System.out.println(prc.numberOfOpenSites());
-        System.out.println(prc.percolates());
+        PercolationStats m = new PercolationStats(200, 100);
+        System.out.println(m.mean());
+        System.out.println(m.stddev());
+        System.out.println(m.confidenceLo());
+        System.out.println(m.confidenceHi());
     }
 }
