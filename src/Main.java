9afb4ec4432866e92e0cb8c17f5c109fc778e1b5
Daniel <danya.ostap@gmail.com>
2022-03-28 16:19:08 +0300

    created PercolationStats class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
+        Percolation prc = new Percolation(4);
+        prc.open(1, 3);
+        prc.open(2, 1);
+        prc.open(2, 2);
+        prc.open(2, 4);
+        prc.open(3, 2);
+        prc.open(3, 3);
+        prc.open(4, 3);
+        prc.open(4, 4);
 
+        //
+        prc.open(2, 3);
+        System.out.println(prc.numberOfOpenSites());
+        System.out.println(prc.percolates());
     }
 }
---
+++ src/PercolationStats.java
@@ -0,0 +1,31 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import java.lang.Math;
+
+public class PercolationStats {
+    private final int T;
+    private final double shu = 1.96;
+    private final double meanVal;
+    private final double stdDev;
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
+        T = trials;
+        double[] threshold = new double[T];
+        for (int i = 0; i < trials; i++) {
+            Percolation prc = new Percolation(n);
+            while (!prc.percolates()) {
+                int r = StdRandom.uniform(n) + 1;
+                int c = StdRandom.uniform(n) + 1;
+                prc.open(r, c);
+            }
+            threshold[i] = (double) prc.numberOfOpenSites() / (n * n);
+        }
+        meanVal = StdStats.mean(threshold);
+        stdDev = StdStats.stddev(threshold);
+    }
+}
